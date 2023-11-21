97818d9096c7d2deede074bae5b672b588adb183
faustitoRoman <78981963+faustitoRoman@users.noreply.github.com>
2023-11-20 22:39:42 -0500

    Declaracion de funciones y uso de funciones

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -29,4 +29,47 @@ fun main(){
     val mayorEdad: Boolean = true
     // CLases java
     val fechaNacimiento: Date = Date()
+
+    //SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No"
+
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 15.00, 20.00)
+    calcularSueldo(10.00, bonoEspecial = 20.00)
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00)
+
+}
+
+//void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    //"Nombre : " + nombre
+    println("Nombre: ${nombre}") //template string
+}
+
+fun calcularSueldo(
+    sueldo: Double, //Requerido
+    tasa: Double = 12.00, //Opcional (defecto)
+    bonoEspecial: Double? = null, //Opcion null -> nullable
+    ): Double{
+        //Int -> Int? (nullable)
+        //String -> String? (nullable)
+        //Date -> Date? (nullable)
+    if (bonoEspecial == null){
+        return sueldo * (100/tasa)
+    } else {
+        return sueldo * (100/tasa) + bonoEspecial
+    }
 }
