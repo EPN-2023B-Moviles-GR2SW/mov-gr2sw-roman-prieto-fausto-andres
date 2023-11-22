2228401476c6dee0f6dd0aaddc0dc5c8fc8a2264
faustitoRoman <78981963+faustitoRoman@users.noreply.github.com>
2023-11-22 09:12:14 -0500

    Update Main.kt
    Se agregaron bloques de código en la sección main con operadores any, all y reduce.

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -115,6 +115,41 @@ fun main(){
     }
     println(respuestaFilter)
     println(respuestaFilterDos)
+
+    //OR AND
+    //OR -> ANY (Alguno cumple?)
+    //AND -> ALL (Todos cumple?)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any { valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) //true
+
+    val respuestaAll: Boolean = arregloDinamico
+        .all { valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll)//false
+
+    //REDUCE -> Valor acumulado
+    //Valor acumulado = 0 (Siempre 0 en lenguaje Kotlin)
+    // [1,2,3,4,5] -> Sume todos los valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1 ->Iteracion 1
+    // valorIteracion2 = valorIteracion1 + 2 = 1 + 2 = 3 ->Iteracion 2
+    // valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 ->Iteracion 3
+    // valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 ->Iteracion 4
+    // valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 ->Iteracion 5
+
+    val respuestaReduce: Int = arregloDinamico
+        .reduce {
+            acumulado: Int, valorActual: Int ->
+            return@reduce (acumulado + valorActual) // Logica negocio
+        }
+    println(respuestaReduce) //78
+
+
+
 }
 
 abstract class NumerosJava{
